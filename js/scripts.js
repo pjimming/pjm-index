7594fbb41b386b17fb371b45e09405b88e20dd4b
panjiangming <panjiangming@qiniu.com>
2023-07-27 18:20:17 +0800

    更改项目结构

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/*!
+* Start Bootstrap - Index v1.0.0 (https://pjmcode.top)
+* Copyright 2023-2023 panjiangming
+* Licensed under MIT (https://github.com/pjimming/pjm-index/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+function getCopyrightYear() {
+    var startYear = 2022;
+    var endYear = new Date().getFullYear();
+    if (startYear === endYear) {
+        return startYear;
+    }
+    return startYear + "-" + endYear;
+}
+
+window.onload = function () {
+    document.getElementById("copyrightYear").textContent = getCopyrightYear();
+};
